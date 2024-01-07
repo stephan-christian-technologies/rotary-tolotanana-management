450b79aa9b5305dbacc0afda8c5e3c5104b50721
Stéphan CHRISTIAN <stephan.christian@versusmind.eu>
2024-01-08 00:08:32 +0300

    Feat: Added patient selection in the list

--- lib/views/pages/patient_list_view.dart
+++ lib/views/pages/patient_list_view.dart
@@ -1,11 +1,11 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 import 'package:rc_rtc_tolotanana/models/edition.dart';
 import 'package:rc_rtc_tolotanana/models/patient.dart';
 import 'package:rc_rtc_tolotanana/services/database.dart';
 import 'package:rc_rtc_tolotanana/utils/csv_utils.dart';
-import 'package:rc_rtc_tolotanana/views/widgets/add_textfield.dart';
 
 class PatientListView extends StatefulWidget {
   const PatientListView({
@@ -20,6 +20,10 @@ class PatientListView extends StatefulWidget {
 
 class _PatientListViewState extends State<PatientListView> {
   late TextEditingController _searchController;
+  bool _isSelecting = false;
+  bool _allSelected = false;
+  List<String> _selectedIds = [];
+  List<Patient> _patients = [];
 
   @override
   void initState() {
@@ -39,7 +43,9 @@ class _PatientListViewState extends State<PatientListView> {
       appBar:
           //app bar with search icon, on press search icon, show search bar
           AppBar(
-        title: const Text('Liste des patients'),
+        title: Text(_isSelecting
+            ? '${_selectedIds.length} séléctionné(s)'
+            : 'Liste des patients'),
         actions: <Widget>[
           //export data to csv
           IconButton(
@@ -48,6 +54,34 @@ class _PatientListViewState extends State<PatientListView> {
               await patientsByEditionToCsv(widget.edition, context);
             },
           ),
+          if (_isSelecting)
+            TextButton(
+                onPressed: () {
+                  setState(() {
+                    _allSelected = !_allSelected;
+                    _selectedIds = [];
+                    if (_allSelected) {
+                      for (Patient patient in _patients) {
+                        _selectedIds.add(patient.id);
+                      }
+                    }
+                  });
+                },
+                child: Text(
+                    (_allSelected && _selectedIds.isNotEmpty)
+                        ? 'Désélectionner tout'
+                        : 'Sélectionner tout',
+                    style: const TextStyle(color: Colors.black))),
+          if (_isSelecting)
+            TextButton(
+                onPressed: () {
+                  setState(() {
+                    _isSelecting = false;
+                    _selectedIds = [];
+                  });
+                },
+                child: const Text('Annuler',
+                    style: TextStyle(color: Colors.black))),
         ],
       ),
       body: Stack(
@@ -76,7 +110,7 @@ class _PatientListViewState extends State<PatientListView> {
                 },
               )),
           Container(
-              margin: const EdgeInsets.only(top: 100),
+              margin: const EdgeInsets.only(top: 90),
               child: patientListWidget()),
         ],
       ),
@@ -102,87 +136,133 @@ class _PatientListViewState extends State<PatientListView> {
                         const Divider(),
                     itemBuilder: (BuildContext context, int index) {
                       Patient patient = snapshot.data![index];
+                      _patients = snapshot.data!;
+                      return Slidable(
+                        key: Key(patient.id),
+                        direction: Axis.horizontal,
+                        useTextDirection: false,
+                        endActionPane: const ActionPane(
+                          // A motion is a widget used to control how the pane animates.
+                          motion: ScrollMotion(),
 
-                      return ListTile(
-                        leading: Text(formatFolderId(patient.folderId),
-                            style: TextStyle(
-                                fontSize: 20,
-                                color: patient.sex == 0
-                                    ? Colors.blue[800]
-                                    : Colors.pink[800])),
-                        title: Row(
+                          // A pane can dismiss the Slidable.
+
+                          // All actions are defined in the children parameter.
                           children: [
-                            Container(
-                              width: 10,
-                              height: 10,
-                              decoration: BoxDecoration(
-                                color: patient.observation == 1
-                                    ? Colors.green[500]
-                                    : Colors.red[500],
-                                borderRadius: BorderRadius.circular(50),
-                              ),
+                            // A SlidableAction can have an icon and/or a label.
+                            SlidableAction(
+                              onPressed: null,
+                              backgroundColor: Color(0xFFFE4A49),
+                              foregroundColor: Colors.white,
+                              icon: Icons.delete,
+                              label: 'Delete',
                             ),
-                            const SizedBox(width: 10),
-                            SizedBox(
-                              width: MediaQuery.of(context).size.width * 0.55,
-                              child: Text(
-                                '${patient.lastname} ${patient.firstname ?? ''} - ${patient.age} ans',
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                                softWrap: true,
-                              ),
+                            SlidableAction(
+                              onPressed: null,
+                              backgroundColor: Color(0xFF21B7CA),
+                              foregroundColor: Colors.white,
+                              icon: Icons.share,
+                              label: 'Share',
                             ),
                           ],
                         ),
-                        subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            FutureBuilder(
-                              future: getOperations(patient.id),
-                              builder: (BuildContext context,
-                                  AsyncSnapshot<String> snapshot) {
-                                if (snapshot.hasError) {
-                                  return const Text(
-                                      'Erreur de chargement de la liste des opérations');
-                                } else if (snapshot.hasData) {
-                                  return Row(
-                                    children: [
-                                      Text(snapshot.data!),
-                                      const SizedBox(width: 10),
-                                      Container(
-                                        padding: const EdgeInsets.all(6),
-                                        decoration: BoxDecoration(
-                                          color: Colors.grey[300],
-                                          borderRadius:
-                                              BorderRadius.circular(5),
-                                        ),
-                                        child: Center(
-                                          child: Text(
-                                            patient.anesthesiaType
-                                                .toString()
-                                                .split('.')
-                                                .last
-                                                .toUpperCase(),
-                                            style:
-                                                const TextStyle(fontSize: 10),
-                                          ),
-                                        ),
-                                      ),
-                                    ],
-                                  );
-                                } else {
-                                  return const Text('Chargement...');
-                                }
-                              },
-                            ),
-                          ],
-                        ),
-                        trailing: IconButton(
-                          icon: const Icon(Icons.delete),
-                          onPressed: () async {
-                            // await DatabaseClient().deletePatient(patient.id);
-                            setState(() {});
+                        child: GestureDetector(
+                          onLongPress: () {
+                            setState(() {
+                              _isSelecting = true;
+                              _selectedIds.add(patient.id);
+                            });
                           },
+                          child: ListTile(
+                            leading: Text(formatFolderId(patient.folderId),
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: patient.sex == 0
+                                        ? Colors.blue[800]
+                                        : Colors.pink[800])),
+                            title: Row(
+                              children: [
+                                Container(
+                                  width: 10,
+                                  height: 10,
+                                  decoration: BoxDecoration(
+                                    color: patient.observation == 1
+                                        ? Colors.green[500]
+                                        : Colors.red[500],
+                                    borderRadius: BorderRadius.circular(50),
+                                  ),
+                                ),
+                                const SizedBox(width: 10),
+                                SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.55,
+                                  child: Text(
+                                    '${patient.lastname} ${patient.firstname ?? ''} - ${patient.age} ans',
+                                    maxLines: 2,
+                                    overflow: TextOverflow.ellipsis,
+                                    softWrap: true,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                FutureBuilder(
+                                  future: getOperations(patient.id),
+                                  builder: (BuildContext context,
+                                      AsyncSnapshot<String> snapshot) {
+                                    if (snapshot.hasError) {
+                                      return const Text(
+                                          'Erreur de chargement de la liste des opérations');
+                                    } else if (snapshot.hasData) {
+                                      return Row(
+                                        children: [
+                                          Text(snapshot.data!),
+                                          const SizedBox(width: 10),
+                                          Container(
+                                            padding: const EdgeInsets.all(6),
+                                            decoration: BoxDecoration(
+                                              color: Colors.grey[300],
+                                              borderRadius:
+                                                  BorderRadius.circular(5),
+                                            ),
+                                            child: Center(
+                                              child: Text(
+                                                patient.anesthesiaType
+                                                    .toString()
+                                                    .split('.')
+                                                    .last
+                                                    .toUpperCase(),
+                                                style: const TextStyle(
+                                                    fontSize: 10),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      );
+                                    } else {
+                                      return const Text('Chargement...');
+                                    }
+                                  },
+                                ),
+                              ],
+                            ),
+                            trailing: _isSelecting
+                                ? Checkbox(
+                                    value: _selectedIds.contains(patient.id),
+                                    onChanged: (bool? value) {
+                                      setState(() {
+                                        if (value == true) {
+                                          _selectedIds.add(patient.id);
+                                        } else {
+                                          _selectedIds.remove(patient.id);
+                                        }
+                                      });
+                                    },
+                                  )
+                                : const Icon(Icons.chevron_right),
+                          ),
                         ),
                       );
                     },
