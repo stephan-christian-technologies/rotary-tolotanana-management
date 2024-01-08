2d38bc3ab1e4b692c443ddc28d82a41efe971009
Stéphan CHRISTIAN <stephan.christian@versusmind.eu>
2024-01-08 17:37:20 +0300

    Chore: added appcenter post-clone

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 
+import 'package:appcenter_sdk_flutter/appcenter_sdk_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:rc_rtc_tolotanana/firebase_options.dart';
 import 'package:rc_rtc_tolotanana/services/database.dart';
@@ -12,6 +14,10 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  final ios = defaultTargetPlatform == TargetPlatform.iOS;
+  var app_secret = ios ? '' : "2188eb3f-c4e4-43f5-ba29-de9e84aa52e8";
+  await AppCenter.start(secret: app_secret);
 
   DatabaseClient databaseClient = DatabaseClient();
 
